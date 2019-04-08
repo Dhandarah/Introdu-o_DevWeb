e1b32cbaad4bc9184d7a936d6d581fe02792fa2f
Dhandarah <dhandarah1996@gmail.com>
2019-04-08 14:40:59 -0300

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+alert("Seja Bem Vindo!");
+function	mostraAlerta() {
+		alert("Funciona!");
+}
+//	obtendo	um	elemento	através	de	um	seletor	de	ID
+var	botao	=	document.querySelector("#botaoEnviar");
+botao.onclick	=	mostraAlerta;
+
+			
